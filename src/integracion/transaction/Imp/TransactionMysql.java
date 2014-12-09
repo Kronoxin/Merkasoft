436c293e5256b148627616bdc248633384050a24
usuario_local <usuario_local@PTO0917>
2014-12-09 17:10:52 +0100

    Start implementado

--- src/integracion/transaction/Imp/TransactionMysql.java
+++ src/integracion/transaction/Imp/TransactionMysql.java
@@ -5,8 +5,14 @@
  */
 package integracion.transaction.Imp;
 
+import integracion.DAO.factoriaDAO.Imp.FactoriaDAOImp;
 import integracion.transaction.Transaction;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  *
@@ -14,14 +20,46 @@ import java.sql.Connection;
  */
 public class TransactionMysql implements Transaction
 {
-
+    
+    private Connection _connection;
+    
     @Override
-    public void start() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void start() 
+    {
+        Properties props = new Properties();
+        try 
+        {
+            InputStream fichero = FactoriaDAOImp.class.getClassLoader().getResourceAsStream("database.properties");
+            props.load(fichero);
+	}
+	catch(IOException ioe) 
+        {
+            throw new RuntimeException("No se ha podido leer el fichero de configuración");
+        }
+        try 
+        {
+            _connection = DriverManager.getConnection(props.getProperty("jdbc.url"), props.getProperty("jdbc.username"), props.getProperty("jdbc.password"));
+	}
+	
+        catch (SQLException e) 
+        {
+
+            throw new RuntimeException("No se ha podido establecer la conexón con la base de datos.");
+	}
+		
+	try 
+        {
+            _connection.setAutoCommit(false);
+	} 
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+	}
     }
 
     @Override
-    public void commit() {
+    public void commit() 
+    {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -31,8 +69,9 @@ public class TransactionMysql implements Transaction
     }
 
     @Override
-    public Connection getResource() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Connection getResource() 
+    {
+        return _connection;
     }
 
     @Override
