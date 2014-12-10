1812cd09f1ec6788a9c083421aa719357ffa0805
usuario_local <usuario_local@PTO0505>
2014-12-10 19:06:50 +0100

    factoriaComm

---
+++ src/presentacion/Controlador/Comandos/venta/GUI/comandoGUImostrarListaVentas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package presentacion.Controlador.Comandos.venta.GUI;
+
+/**
+ *
+ * @author usuario_local
+ */
+public class comandoGUImostrarListaVentas {
+    
+}
