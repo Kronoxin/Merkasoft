c0205486184ee7942813d33a1ad124c9961c2d95
Marina <Marina@HP>
2014-12-10 22:53:00 +0100

    Dispatcher agregado

---
+++ src/presentacion/Controlador/dispatcher/Dispatcher.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentacion.Controlador.dispatcher;
+
+import presentacion.Controlador.Comandos.RespuestaComando;
+import presentacion.Controlador.dispatcher.Imp.DispatcherImp;
+
+/**
+ *
+ * @author Marina
+ */
+public abstract class Dispatcher 
+{
+    public static Dispatcher _instance = null;
+    
+    public static Dispatcher getInstance()
+    {
+        if (_instance == null)
+            _instance = new DispatcherImp();
+        
+        return _instance;
+    }
+    
+    public abstract void dispatch(RespuestaComando comando);
+}
---
+++ src/presentacion/Controlador/dispatcher/Imp/DispatcherImp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentacion.Controlador.dispatcher.Imp;
+
+import presentacion.Controlador.Comandos.RespuestaComando;
+import presentacion.Controlador.dispatcher.Dispatcher;
+
+/**
+ *
+ * @author Marina
+ */
+public class DispatcherImp extends Dispatcher
+{
+
+    @Override
+    public void dispatch(RespuestaComando comando) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
