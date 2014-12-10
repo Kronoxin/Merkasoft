548f909cd2894b1d3fd80709824aa5da339dfb55
Guinos <Guinos@192.168.1.156>
2014-12-10 01:08:47 +0100

    pruebas jdbc

--- src/integracion/transaction/Imp/TransactionMysql.java
+++ src/integracion/transaction/Imp/TransactionMysql.java
@@ -28,21 +28,23 @@ public class TransactionMysql implements Transaction
     @Override
     public void start() throws Exception
     {
-        Properties props = new Properties();
+        /*Properties props = new Properties();
         try 
         {
             // Cargamos la configuracion de la BBDD.
-            InputStream fichero = FactoriaDAOImp.class.getClassLoader().getResourceAsStream("database.properties");
+            InputStream fichero = Thread.currentThread().getContextClassLoader().getResourceAsStream("database.properties");
             props.load(fichero);
 	}
 	catch(IOException ioe) 
         {
             throw new RuntimeException("No se ha podido leer el fichero de configuración");
-        }
+        }*/
         try 
         {
-            _connection = DriverManager.getConnection(props.getProperty("jdbc.url"), props.getProperty("jdbc.username"), props.getProperty("jdbc.password"));
+            _connection = DriverManager.getConnection("jdbc:mysql://localhost/MerkaSoft", "root", "");
+           // _connection = DriverManager.getConnection(props.getProperty("jdbc.url"), props.getProperty("jdbc.username"), props.getProperty("jdbc.password"));
 	}
+        
 	
         catch (SQLException e) 
         {
