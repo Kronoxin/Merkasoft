7beccc7b0bb69fe01e90e217a69cc157815213f3
usuario_local <usuario_local@PTO1009>
2015-01-22 10:40:23 +0100

    Turno cambiado

--- src/presentacion/Controlador/Comandos/turno/comandoModificarTurno.java
+++ src/presentacion/Controlador/Comandos/turno/comandoModificarTurno.java
@@ -6,6 +6,7 @@
 package presentacion.Controlador.Comandos.turno;
 
 import negocio.FactoriaSA.FactoriaSA;
+import negocio.turnos.Turno;
 import presentacion.Controlador.Comandos.Command;
 import presentacion.Controlador.Comandos.RespuestaComando;
 import presentacion.Controlador.Eventos.EventoNegocio;
@@ -18,7 +19,7 @@ public class comandoModificarTurno implements Command{
 
     @Override
     public RespuestaComando execute(Object datos) {
-   /*     boolean correcto = FactoriaSA.obtenerInstancia().generaSATurno().modificarTurno((Turno)datos);
+        boolean correcto = FactoriaSA.obtenerInstancia().generaSATurno().modificarTurno((Turno)datos);
 	RespuestaComando respuesta;
 		
 	if (correcto)
@@ -29,10 +30,10 @@ public class comandoModificarTurno implements Command{
         {
             respuesta = new RespuestaComando(EventoNegocio.FRACASO_MODIFICAR_TURNO, (Turno)datos);
 	}
-	return respuesta;*/
+	return respuesta;
         
         //puesto aqui hasta que no esté completo el SA, para poder ejecutar la aplicacion
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
